85789570352c76c2de7a7777a5c2c047f1a09e99
Hoang Man <hoangman123456789p@gmail.com>
2023-03-02 09:42:22 +0700

    first commit

---
+++ WebsiteBanHang/WebsiteBanHang/Controllers/Listing_gridController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebsiteBanHang.Controllers
+{
+    public class Listing_gridController : Controller
+    {
+        // GET: Listing_grid
+        public ActionResult grid()
+        {
+            return View();
+        }
+    }
+}
