6bcbbab6ed374ff4100e6e9dffe7d2d45e467132
Hoang Man <hoangman123456789p@gmail.com>
2023-03-30 10:07:19 +0700

    themmoi30

--- WebsiteBanHang/WebsiteBanHang/Areas/Admin/Controllers/ProductController.cs
+++ WebsiteBanHang/WebsiteBanHang/Areas/Admin/Controllers/ProductController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.IO;
 using System.Linq;
 using System.Web;
@@ -21,9 +23,20 @@ namespace WebsiteBanHang.Areas.Admin.Controllers
         [HttpGet]
         public ActionResult Create()
         {
-            
+           
             return View();
         }
+
+        private dynamic ToSelectiList(DataTable dtCategory, string v1, string v2)
+        {
+            throw new NotImplementedException();
+        }
+
+        private dynamic ToSelectList(DataTable dtCategory, string v1, string v2)
+        {
+            throw new NotImplementedException();
+        }
+
         [HttpPost]
         public ActionResult Create(Product objProduct)
         {
@@ -35,6 +48,7 @@ namespace WebsiteBanHang.Areas.Admin.Controllers
                     string extension = Path.GetExtension(objProduct.ImageUpload.FileName);
                     fileName = fileName + "_" + long.Parse(DateTime.Now.ToString("yyyyMMddhhmmss"))+ extension;
                     objProduct.Avatar = fileName;
+                    //lưu file hình
                     objProduct.ImageUpload.SaveAs(Path.Combine(Server.MapPath("~/Content/images/"), fileName));
                 }
                 dbObj.Products.Add(objProduct);
@@ -53,6 +67,41 @@ namespace WebsiteBanHang.Areas.Admin.Controllers
         {
             var objProduct = dbObj.Products.Where(n => n.Id == id).FirstOrDefault();
             return View(objProduct);
+        }
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            var objProduct = dbObj.Products.Where(n => n.Id == id).FirstOrDefault();
+            return View(objProduct);
+        }
+        [HttpPost]
+        public ActionResult Delete(Product objPro)
+        {
+            var objProduct = dbObj.Products.Where(n => n.Id == objPro.Id).FirstOrDefault();
+            dbObj.Products.Remove(objProduct);
+            dbObj.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var objProduct = dbObj.Products.Where(n => n.Id == id).FirstOrDefault();
+            return View(objProduct);
+        }
+        [HttpPost]
+        public ActionResult Edit(int id,Product objProduct)
+        {
+            if (objProduct.ImageUpload != null)
+            {
+                string fileName = Path.GetFileNameWithoutExtension(objProduct.ImageUpload.FileName);
+                string extension = Path.GetExtension(objProduct.ImageUpload.FileName);
+                fileName = fileName + "_" + long.Parse(DateTime.Now.ToString("yyyyMMddhhmmss")) + extension;
+                objProduct.Avatar = fileName;
+                objProduct.ImageUpload.SaveAs(Path.Combine(Server.MapPath("~/Content/images/"), fileName));
+            }
+            dbObj.Entry(objProduct).State = EntityState.Modified;
+            dbObj.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
